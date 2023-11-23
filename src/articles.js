f9ca0ac7ed31c37bc18ae97b0e7e3367584314f5
Ophelie <freville.ophelie@neuf.fr>
2023-11-23 13:47:56 +0100

    Re-création Gallery et Cards

--- src/articles.js
+++ src/articles.js
@@ -1,53 +1,53 @@
 const articles = [
     {
-        id: 1,
+        id: 0,
         name: "Bananas",
-        img: '../public/assets/img/coursesYiga.jpg',
+        img: './assets/img/coursesYiga.jpg',
         description: "Vous êtes un Yiga ? Vous A-D-O-R-E-Z les bananes ! Ici, on les vend en gros !",
         price: 245.0,
         qte: 27,
         promo: false
     },
     {
-        id: 2,
+        id: 1,
         name: "Organiseur pour brodeuse",
-        img: '../public/assets/img/threadsOrganizer.jpg',
+        img: './assets/img/threadsOrganizer.jpg',
         description: "Marre des échevettes qui trainent et s\'emmêlent. Cette aiguille a encore disparu ? Pas de problème, voici LA solution ! Fabrication artisanale, en bois véritable.",
         price: 25.0,
         qte: 42,
         promo: false
     },
     {
-        id: 3,
+        id: 2,
         name: "Cadre Nostalgique",
-        img: '../publiuc/assets/img/oldHelper.jpg',
+        img: './assets/img/oldHelper.jpg',
         description: "Votre petit assistant virtuel vous manque ? Redtrouvez-le grâce à ces ouvrages artisanaux fabriqués par nos brodeuses",
         price: 20.0,
         qte: 36,
         promo: false
     },
     {
-        id: 4,
+        id: 3,
         name: "Faux Ongles",
-        img: '../publiuc/assets/img/poseVernis.jpg',
+        img: './assets/img/poseVernis.jpg',
         description: "Faux ongles X 10 , motifs aux choix (fournis sans la colle)",
         price: 10.0,
         qte: 17.0,
         promo: true
     },
     {
-        id: 5,
+        id: 4,
         name: "Snacks",
-        img: '../publiuc/assets/img/snacksOverflow.jpg',
+        img: './assets/img/snacksOverflow.jpg',
         description: "Snacks aux choix. ATTENTION ! Uiquement en vente à emporter ! Livraison indisponible !",
         price: 1.5,
         qte: 92,
         promo: true
     },
     {
-        id: 6,
+        id: 5,
         name: "Bicyclette Waterproof",
-        img: "../publiuc/assets/img/veloWaterPlouf.jpg",
+        img: "./assets/img/veloWaterPlouf.jpg",
         description: "Comme son nom l'indique, ce vélo fonctionne sous l'eau. Utilisation d'équipement de plongée recommandée !!! (Voir section 'Sport' de notre site)",
         price: 435.0,
         qte: 2,
---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Gallery.css';
+
+const Gallery = () => (
+  <div className="Gallery">
+    Gallery Component
+  </div>
+);
+
+Gallery.propTypes = {};
+
+Gallery.defaultProps = {};
+
+export default Gallery;
