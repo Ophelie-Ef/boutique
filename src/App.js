f388ab1e67a073fc7c42d4b2b01492a2e297edbd
Ophelie <freville.ophelie@neuf.fr>
2023-11-23 12:22:00 +0100

    Création des components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-const App = ()=>{
-  return(
-    <></>
+const App = () => {
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
   )
 }
 
