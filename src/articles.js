7499a1e10a69bbe62cee9f6021573fdcdc8bf6d5
Ophelie <freville.ophelie@neuf.fr>
2023-11-23 12:09:38 +0100

    Tableau d'objets terminé

---
+++ src/articles.js
@@ -0,0 +1,58 @@
+const articles = [
+    {
+        id: 1,
+        name: "Bananas",
+        img: '../public/assets/img/coursesYiga.jpg',
+        description: "Vous êtes un Yiga ? Vous A-D-O-R-E-Z les bananes ! Ici, on les vend en gros !",
+        price: 245.0,
+        qte: 27,
+        promo: false
+    },
+    {
+        id: 2,
+        name: "Organiseur pour brodeuse",
+        img: '../public/assets/img/threadsOrganizer.jpg',
+        description: "Marre des échevettes qui trainent et s\'emmêlent. Cette aiguille a encore disparu ? Pas de problème, voici LA solution ! Fabrication artisanale, en bois véritable.",
+        price: 25.0,
+        qte: 42,
+        promo: false
+    },
+    {
+        id: 3,
+        name: "Cadre Nostalgique",
+        img: '../publiuc/assets/img/oldHelper.jpg',
+        description: "Votre petit assistant virtuel vous manque ? Redtrouvez-le grâce à ces ouvrages artisanaux fabriqués par nos brodeuses",
+        price: 20.0,
+        qte: 36,
+        promo: false
+    },
+    {
+        id: 4,
+        name: "Faux Ongles",
+        img: '../publiuc/assets/img/poseVernis.jpg',
+        description: "Faux ongles X 10 , motifs aux choix (fournis sans la colle)",
+        price: 10.0,
+        qte: 17.0,
+        promo: true
+    },
+    {
+        id: 5,
+        name: "Snacks",
+        img: '../publiuc/assets/img/snacksOverflow.jpg',
+        description: "Snacks aux choix. ATTENTION ! Uiquement en vente à emporter ! Livraison indisponible !",
+        price: 1.5,
+        qte: 92,
+        promo: true
+    },
+    {
+        id: 6,
+        name: "Bicyclette Waterproof",
+        img: "../publiuc/assets/img/veloWaterPlouf.jpg",
+        description: "Comme son nom l'indique, ce vélo fonctionne sous l'eau. Utilisation d'équipement de plongée recommandée !!! (Voir section 'Sport' de notre site)",
+        price: 435.0,
+        qte: 2,
+        promo: false
+    }
+]
+
+export { articles }
